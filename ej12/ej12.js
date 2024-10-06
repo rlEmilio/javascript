e794f913c779a9a07d2232254f6229656cb17c95
Emilio <ruizlopezemilio2014@gmail.com>
2024-10-06 17:44:10 +0200

    mensaje

---
+++ ej12/ej12.js
@@ -0,0 +1,9 @@
+function filtrarNombres(lista){
+    let filtrado = lista.filter(nombre => nombre.length === 4);
+       
+    console.log(filtrado);
+    }
+
+    filtrarNombres(["Luis","Estela","Angel","Enya","Jose Antonio"]);
+
+
