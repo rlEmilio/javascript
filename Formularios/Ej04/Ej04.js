3ea4abb38601d21584a7bd6f48a831ed4e23955d
rlEmilio <ruizlopezemilio2014@gmail.com>
2024-12-11 13:38:41 +0100

    aaaa

--- Formularios/Ej04/Ej04.js
+++ Formularios/Ej04/Ej04.js
@@ -2,32 +2,43 @@ window.onload = function () {
 
     let formulario = document.formulario;
     //console.log(formulario);
-    let boton = formulario.elements[2];
+    let botonDolares = formulario.elements[2];
     //console.log(boton);
+    let botonEuros = formulario.elements[3];
 
-    boton.addEventListener("click", convertir);
+    botonDolares.addEventListener("click", convertirDolares);
+    botonEuros.addEventListener("click", convertirEuros);
 
 }
 
 
 //funcion que le añado al event listener.
-function convertir() {
+function convertirDolares() {
+
     let cantidad = formulario.elements[0];
     let conversion = formulario.elements[1];
+    let cifra = parseFloat(cantidad.value);
 
-    let cifra = 0;
-    //console.log(cantidad.value);
-    do {
-        let cifra = parseFloat(cantidad.value);
-        console.log(cifra);
-
-    
-
-        if (isNaN(cifra) || cifra <= 0 || cifra == "") {
-            alert("Introduce un número mayor a 0, por favor");
-        }
-    } while (isNaN(cifra) || cifra <= 0 || cifra == "")
-
+    if (isNaN(cifra) || cifra <= 0) {
+        alert("Por favor, introduce un número válido");
+    } else {
+        conversion.value = (cifra * 1.17).toFixed(2) + "$";
+    }
 
 
 }
+
+function convertirEuros() {
+
+  
+    let cantidad = formulario.elements[0];
+    let conversion = formulario.elements[1];
+    let cifra = parseFloat(cantidad.value);
+    if (isNaN(cifra) || cifra <= 0) {
+        alert("Por favor, introduce un número válido");
+    } else {
+        conversion.value = (cifra * 0.83).toFixed(2) + "€";
+    }
+
+}
+
