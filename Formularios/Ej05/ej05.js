3c7a7034c0e30314ccef520c3d568cf55f0ff82e
Emilio <ruizlopezemilio2014@gmail.com>
2025-01-09 19:24:13 +0100

    aaa

--- Formularios/Ej05/ej05.js
+++ Formularios/Ej05/ej05.js
@@ -14,23 +14,29 @@ function comprobar(formulario) {
   //compruebo si los campos están vacíos
   if (campo1.value == "" && campo2.value == "") {
     alert("Los campos están vacíos");
-    //compruebo tamaño
- }else{
+
+ }else if(campo1.value.toLowerCase() === campo2.value.toLowerCase()){
+  alert("No son anagramas");
+ }
+ else
+ {
    let string_sin_espacios = campo1.value.toLowerCase().replaceAll(" ","");
-   //console.log(string_sin_espacios);
    //dividir string por letras y ordenar alfabeticamente
    let letras = string_sin_espacios.split("").sort();
    let string_ordenado = letras.join("");
    console.log(string_ordenado);
   
    let string_sin_espacios2 = campo2.value.toLowerCase().replaceAll(" ","");
-   //console.log(string_sin_espacios);
    //dividir string por letras y ordenar alfabeticamente
    let letras2 = string_sin_espacios2.split("").sort();
    let string_ordenado2 = letras2.join("");
    console.log(string_ordenado2);
   
-   
+   if(string_ordenado === string_ordenado2){
+    alert("Son anagramas");
+   }else{
+    alert("No son anagramas");
+   }
     
  }
   
