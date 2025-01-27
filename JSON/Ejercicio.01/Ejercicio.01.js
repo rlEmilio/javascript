5733df3dab54b85a256e1e3b035db6a939fae8be
rlEmilio <ruizlopezemilio2014@gmail.com>
2025-01-27 14:46:15 +0100

    aaaaaaaaaa

---
+++ JSON/Ejercicio.01/Ejercicio.01.js
@@ -0,0 +1,16 @@
+const tutor = {
+
+    nombre:"Luis",
+    edad:48,
+    DNI:"12345678Q",
+    titulo_universitario:"Desarrollo web"
+
+};
+
+const asignatura = {
+    nombre:"Cliente",
+    curso:2,
+    horas:300
+
+};
+
